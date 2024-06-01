e3d0d6cbd808a28ee255a17717a298de36bb1793
Zygmut <deltas.game@gmail.com>
2024-06-01 23:15:22 +0200

    added pre-commit

--- shell.nix
+++ shell.nix
@@ -1,10 +1,11 @@
 # We pin to a specific nixpkgs commit for reproducibility.
-{ pkgs? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11") {} }:
+{ pkgs? import (fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05") {} }:
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
     docker-compose
     jq
+    pre-commit
     (python3.withPackages (ps: with ps; [
       requests
       beautifulsoup4
@@ -20,5 +21,7 @@ pkgs.mkShellNoCC {
     sleep 0.5
 
     export INFLUX_TOKEN=$(docker exec -t "influxdb" influx auth list --user admin --json | jq -r ".[0].token")
+
+    pre-commit install
   '';
 }
